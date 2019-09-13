ae27b16c7e52fb14eca3f7e456a8c0315203dc9e
dam360210 <213dollar@gmail.com>
2019-09-13 13:54:25 +0900

    init

---
+++ data.js
@@ -0,0 +1,340 @@
+
+var ganttData =[
+
+{
+    name: "01号機",
+    desc: "<font color=red>予定</font>",
+    values: [
+        {
+            from: "/Date(1322608333000)/",
+            to: "/Date(1322614213000)/",
+            label: "G051234__<br>012394029332",
+            customClass: "ganttGreen"
+        },
+        {
+            from: "/Date(1322614213000)/",
+            to: "/Date(1322631253000)/",
+            label: "1-2",
+            customClass: "ganttGreen"
+        }
+    ]
+},
+{
+    name: "",
+    desc: "実績",
+    values: [
+        {
+            from: "/Date(1322611250000)/",
+            to: "/Date(1322631250000)/",
+            label: "1-3",
+            customClass: "ganttGreen1"
+        },
+        {
+            from: "/Date(1322631250000)/",
+            to: "/Date(1322661250000)/",
+            label: "1-4",
+            customClass: "ganttGreen1"
+        }
+    ]
+},
+{
+    name: "02号機",
+    desc: "<font color=red>予定</font>",
+    values: []
+},
+{
+    name: "",
+    desc: "実績",
+    values: []
+},
+{
+    name: "03号機",
+    desc: "<font color=red>予定</font>",
+    values: [
+        {
+            from: "/Date(1322611250000)/",
+            to: "/Date(1322631250000)/",
+            label: "3-1",
+            customClass: "ganttGreen"
+        },
+        {
+            from: "/Date(1322631250000)/",
+            to: "/Date(1322661250000)/",
+            label: "3-2",
+            customClass: "ganttGreen"
+        }
+    ]
+},
+{
+    name: "",
+    desc: "実績",
+    values: [
+        {
+            from: "/Date(1322611250000)/",
+            to: "/Date(1322631250000)/",
+            label: "3-3",
+            customClass: "ganttGreen1"
+        },
+        {
+            from: "/Date(1322631250000)/",
+            to: "/Date(1322661250000)/",
+            label: "3-4",
+            customClass: "ganttGreen1"
+        }
+    ]
+},
+{
+    name: "04号機",
+    desc: "<font color=red>予定</font>",
+    values: [
+        {
+            from: "/Date(1322611250000)/",
+            to: "/Date(1322631250000)/",
+            label: "4-1",
+            customClass: "ganttGreen"
+        },
+        {
+            from: "/Date(1322631250000)/",
+            to: "/Date(1322661250000)/",
+            label: "4-2",
+            customClass: "ganttGreen"
+        }
+    ]
+},
+{
+    name: "",
+    desc: "実績",
+    values: [
+        {
+            from: "/Date(1322611250000)/",
+            to: "/Date(1322631250000)/",
+            label: "4-3",
+            customClass: "ganttGreen1"
+        },
+        {
+            from: "/Date(1322631250000)/",
+            to: "/Date(1322661250000)/",
+            label: "4-4",
+            customClass: "ganttGreen1"
+        }
+        ,
+        {
+            from: "/Date(1322661250000)/",
+            to: "/Date(1322671250000)/",
+            label: "4-5",
+            customClass: "ganttGreen1"
+        }
+        ,
+        {
+            from: "/Date(1322671250000)/",
+            to: "/Date(1322817743000)/",
+            label: "4-6",
+            customClass: "ganttGreen1"
+        }
+    ]
+}
+,
+{
+    name: "05号機",
+    desc: "<font color=red>予定</font>",
+    values: [
+        {
+            from: "/Date(1322611250000)/",
+            to: "/Date(1322631250000)/",
+            label: "4-1",
+            customClass: "ganttGreen"
+        },
+        {
+            from: "/Date(1322631250000)/",
+            to: "/Date(1322661250000)/",
+            label: "4-2",
+            customClass: "ganttGreen"
+        }
+    ]
+},
+{
+    name: "",
+    desc: "実績",
+    values: [
+        {
+            from: "/Date(1322611250000)/",
+            to: "/Date(1322631250000)/",
+            label: "4-3",
+            customClass: "ganttGreen1"
+        },
+        {
+            from: "/Date(1322631250000)/",
+            to: "/Date(1322661250000)/",
+            label: "4-4",
+            customClass: "ganttGreen1"
+        }
+        ,
+        {
+            from: "/Date(1322661250000)/",
+            to: "/Date(1322671250000)/",
+            label: "4-5",
+            customClass: "ganttGreen1"
+        }
+        ,
+        {
+            from: "/Date(1322671250000)/",
+            to: "/Date(1322817743000)/",
+            label: "4-6",
+            customClass: "ganttGreen1"
+        }
+    ]
+}
+,
+{
+    name: "06号機",
+    desc: "<font color=red>予定</font>",
+    values: [
+        {
+            from: "/Date(1322611250000)/",
+            to: "/Date(1322631250000)/",
+            label: "4-1",
+            customClass: "ganttGreen"
+        },
+        {
+            from: "/Date(1322631250000)/",
+            to: "/Date(1322661250000)/",
+            label: "4-2",
+            customClass: "ganttGreen"
+        }
+    ]
+},
+{
+    name: "",
+    desc: "実績",
+    values: [
+        {
+            from: "/Date(1322611250000)/",
+            to: "/Date(1322631250000)/",
+            label: "4-3",
+            customClass: "ganttGreen1"
+        },
+        {
+            from: "/Date(1322631250000)/",
+            to: "/Date(1322661250000)/",
+            label: "4-4",
+            customClass: "ganttGreen1"
+        }
+        ,
+        {
+            from: "/Date(1322661250000)/",
+            to: "/Date(1322671250000)/",
+            label: "4-5",
+            customClass: "ganttGreen1"
+        }
+        ,
+        {
+            from: "/Date(1322671250000)/",
+            to: "/Date(1322817743000)/",
+            label: "4-6",
+            customClass: "ganttGreen1"
+        }
+    ]
+}
+,
+{
+    name: "07号機",
+    desc: "<font color=red>予定</font>",
+    values: [
+        {
+            from: "/Date(1322611250000)/",
+            to: "/Date(1322631250000)/",
+            label: "4-1",
+            customClass: "ganttGreen"
+        },
+        {
+            from: "/Date(1322631250000)/",
+            to: "/Date(1322661250000)/",
+            label: "4-2",
+            customClass: "ganttGreen"
+        }
+    ]
+},
+{
+    name: "",
+    desc: "実績",
+    values: [
+        {
+            from: "/Date(1322611250000)/",
+            to: "/Date(1322631250000)/",
+            label: "4-3",
+            customClass: "ganttGreen1"
+        },
+        {
+            from: "/Date(1322631250000)/",
+            to: "/Date(1322661250000)/",
+            label: "4-4",
+            customClass: "ganttGreen1"
+        }
+        ,
+        {
+            from: "/Date(1322661250000)/",
+            to: "/Date(1322671250000)/",
+            label: "4-5",
+            customClass: "ganttGreen1"
+        }
+        ,
+        {
+            from: "/Date(1322671250000)/",
+            to: "/Date(1322817743000)/",
+            label: "4-6",
+            customClass: "ganttGreen1"
+        }
+    ]
+}
+,
+{
+    name: "08号機",
+    desc: "<font color=red>予定</font>",
+    values: [
+        {
+            from: "/Date(1322611250000)/",
+            to: "/Date(1322631250000)/",
+            label: "4-1",
+            customClass: "ganttGreen"
+        },
+        {
+            from: "/Date(1322631250000)/",
+            to: "/Date(1322661250000)/",
+            label: "4-2",
+            customClass: "ganttGreen"
+        }
+    ]
+},
+{
+    name: "",
+    desc: "実績",
+    values: [
+        {
+            from: "/Date(1322611250000)/",
+            to: "/Date(1322631250000)/",
+            label: "4-3",
+            customClass: "ganttGreen1"
+        },
+        {
+            from: "/Date(1322631250000)/",
+            to: "/Date(1322661250000)/",
+            label: "4-4",
+            customClass: "ganttGreen1"
+        }
+        ,
+        {
+            from: "/Date(1322661250000)/",
+            to: "/Date(1322671250000)/",
+            label: "4-5",
+            customClass: "ganttGreen1"
+        }
+        ,
+        {
+            from: "/Date(1322671250000)/",
+            to: "/Date(1322817743000)/",
+            label: "4-6",
+            customClass: "ganttGreen1"
+        }
+    ]
+}
+]
